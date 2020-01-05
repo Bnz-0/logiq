0e2de543e68a90acaeec920d3cba773d95be3674
Bnz <matteo.benzi97@gmail.com>
2020-01-05 17:41:18 +0100

    Version 0.1.2

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open('LICENSE', encoding='utf-8') as f:
 
 setup(
     name='logiq',
-    version='0.1.1',
+    version='0.1.2',
     description='Logiq, a simple to use quantum simulator library',
     long_description='Github repo: https://github.com/Bnz-0/logiq',
     author='Bnz',
