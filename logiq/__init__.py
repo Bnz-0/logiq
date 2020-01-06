cd21eab0bf2b7714b15ad1a1d465f9a474c572f1
Bnz <matteo.benzi97@gmail.com>
2020-01-06 22:54:39 +0100

    Little bug fixed

--- logiq/__init__.py
+++ logiq/__init__.py
@@ -2,5 +2,5 @@ from .src.abs_Qstate import auto_norm, cheat
 from .src.Basis import Basis, CanonBasis, bell, hadamard, stdbasis
 from .src.Operator import Op
 from .src.Qmath import bra, ket, kron, matrix, nkron, roundedVector, vector
-from .src.Qstate import Qstate, qbit, qdit
+from .src.Qstate import Qstate, qbit
 from .src.qtils import equal, equals, isScalar, set_n_digits, set_precision
