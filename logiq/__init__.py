c8141062243a39321e2ac45a7984fd9c7b055b7e
Bnz <matteo.benzi97@gmail.com>
2020-02-16 12:40:25 +0100

    [fix] added forgot function to import

--- logiq/__init__.py
+++ logiq/__init__.py
@@ -3,4 +3,4 @@ from .src.Basis import Basis, CanonBasis, bell, hadamard, stdbasis
 from .src.Operator import Op
 from .src.Qmath import bra, ket, kron, matrix, nkron, roundedVector, vector
 from .src.Qstate import Qstate, qbit
-from .src.qtils import equal, equals, isScalar, set_n_digits, set_precision
+from .src.qtils import equal, equals, isScalar, set_n_digits, set_precision, what_precision
